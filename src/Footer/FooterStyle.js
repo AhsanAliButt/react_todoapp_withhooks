3368cb2d29ec71bebd3fb7c55b211b2316b7d968
Ahsan Ali <ahsanbutt515@icloud.com>
2022-07-11 21:34:50 +0500

    checked its working

--- src/Footer/FooterStyle.js
+++ src/Footer/FooterStyle.js
@@ -1,16 +1,15 @@
-export const Fstyle={
-    footerdiv:{
-        position:'relative',
-        height: '45px',
-        width: '466px',
-        margin:'3px',
-        marginTop:'15px',
-        overflow:'hidden',
-        color:'pink'
-    },
-    button:{
-        backgroundColor:"red",
-        borderRadius:'10px'
-    }
-
-}
+export const Fstyle = {
+  footerdiv: {
+    position: "relative",
+    height: "45px",
+    width: "466px",
+    margin: "3px",
+    marginTop: "15px",
+    overflow: "hidden",
+    color: "pink",
+  },
+  button: {
+    backgroundColor: "red",
+    borderRadius: "10px",
+  },
+};
